c37362abd6b9b140ed799af3b8406b2d259d1b23
Paul Fischer <63171133+pjfischer@users.noreply.github.com>
2025-02-11 12:25:58 -0500

    Add files via upload

---
+++ stock_returns.R
@@ -0,0 +1,27 @@
+tickers <- c("TSLA","NVDA")
+
+Portfolio <- yf_get(
+  tickers,
+  first_date = Sys.Date() - 300,
+  last_date = Sys.Date(),
+  thresh_bad_data = 0.75,
+  bench_ticker = "^GSPC",
+  type_return = "log",
+  freq_data = "daily",
+  how_to_aggregate = "last",
+  do_complete_data = FALSE,
+  do_cache = TRUE,
+  cache_folder = yf_cachefolder_get(),
+  do_parallel = FALSE,
+  be_quiet = FALSE
+)
+head(Portfolio)
+tail(Portfolio)
+
+cum_log_ret<-Portfolio %>% 
+  group_by(Portfolio$ticker) %>% 
+  summarize(log_ret = sum(ret_closing_prices, na.rm=TRUE))
+cum_log_ret$log_ret
+
+cum_ret <- exp(cum_log_ret$log_ret)-1
+cum_ret
